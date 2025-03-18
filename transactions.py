6811146929fa86401543b80901bc3b2cb643f5ae
AdelinePat <adeline.patenne@hotmail.fr>
2025-03-18 19:20:44 +0100

    refactor: create view folder , create interface class, login class and utiltool class

---
+++ transactions.py
@@ -0,0 +1,12 @@
+import mysql.connector
+import customtkinter
+# from view.interface import Interface
+from view.login_out import LogInOut
+from view.__settings__ import DARK_BLUE, SOFT_BLUE, LIGHT_BLUE, YELLOW, SOFT_YELLOW, PINK
+
+class ViewTransactions():
+    pass
+
+
+login_view = LogInOut("Connexion client", 0)
+login_view.mainloop()
---
+++ view/utiltool.py
@@ -0,0 +1,16 @@
+import customtkinter
+from view.__settings__ import TITLE_FONT_PATH, TEXT_FONT_PATH
+
+class UtilTool():
+    
+    def get_title_font(self, font_size):
+        custom_font = customtkinter.FontManager.load_font(TITLE_FONT_PATH)
+        custom_font = customtkinter.CTkFont(family="Leckerli One", size=font_size)
+        custom_font = ("Leckerli One", font_size)
+        return custom_font
+
+    def get_text_font(self, font_size):
+        custom_font = customtkinter.FontManager.load_font(TEXT_FONT_PATH)
+        custom_font = customtkinter.CTkFont(family="Josefin Sans Regular", size=font_size)
+        custom_font = ("Josefin Sans Regular", font_size)
+        return custom_font
