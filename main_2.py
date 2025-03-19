7b3950e9c92ad8f20dfd1e337bf4b22d625ecfef
AeRiaSaito <florence.navet@laplateforme.io>
2025-03-19 11:54:43 +0100

    import fonction eye close toggle

---
+++ main_2.py
@@ -0,0 +1,18 @@
+import mysql.connector
+import customtkinter
+# import hashlib 
+# import secrets
+
+
+
+# from view.interface import Interface
+from view.login_out import LogInOut
+from view.transactions import TransactionView
+from view.__settings__ import DARK_BLUE, SOFT_BLUE, LIGHT_BLUE, YELLOW, SOFT_YELLOW, PINK
+
+class ViewTransactions():
+    pass
+
+
+login_view = LogInOut("Connexion Client", 0)
+login_view.mainloop()
