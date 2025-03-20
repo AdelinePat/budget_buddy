9336f11244f6fe41a3976a5b6c5d199420b25505
jolyne-mangeot <joseph.mangeot@laplateforme.io>
2025-03-20 09:17:43 +0100

    work in progress: add frames model and set base for historic frame

--- main_2.py
+++ main_2.py
@@ -5,11 +5,24 @@ import secrets
 # from view.interface import Interface
 from view.login_out import LogInOut
 from view.transactions import TransactionView
+from view.interface_frames import Interface_frames
+from view.historic import Historic
 from view.__settings__ import DARK_BLUE, SOFT_BLUE, LIGHT_BLUE, YELLOW, SOFT_YELLOW, PINK
 
 class ViewTransactions():
     pass
 
 
-login_view = TransactionView("Transactions", 0)
+# login_view = TransactionView("Transactions", 0)
+login_view = Historic("HIHIAHHEIAHIIFEZHFEZ", 1)
+
+login_view.interface_frame = Interface_frames(login_view, bg_color=DARK_BLUE, fg_color=LIGHT_BLUE, width=200, corner_radius=20)
+login_view.interface_frame.columnconfigure(0, weight=1)
+login_view.interface_frame.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
+login_view.interface_frame.box = customtkinter.CTkLabel(
+    login_view.interface_frame, text="fegfeer", 
+    height=50, width=200, bg_color=DARK_BLUE, corner_radius=20,
+    fg_color=SOFT_BLUE, text_color=LIGHT_BLUE
+)
+login_view.interface_frame.box.grid(row=0, column=0)
 login_view.mainloop()
