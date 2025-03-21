48b87a2aaa80d130342264d61f1b61c138f1a738
Adeline <adeline.patenne@hotmail.fr>
2025-03-21 13:34:44 +0100

    feat: check date method

--- test.py
+++ test.py
@@ -1,33 +1,33 @@
 from model.server import ServerDatabase
-from datetime import datetime
+from datetime import datetime, date
 
-account = 'jolyne.mangeot@laplateforme.io'
-current_session = 2
+# account = 'jolyne.mangeot@laplateforme.io'
+# current_session = 2
 
-database = ServerDatabase()
+# database = ServerDatabase()
 
-my_database = database.database_connection()
-account_id = 1
+# my_database = database.database_connection()
+# account_id = 1
 
-if my_database.is_connected():
-    cursor = my_database.cursor()
-    query = "SELECT deal_date FROM Transactions WHERE id_account_emitter = 2;"
-    cursor.execute(query)
-    dates = cursor.fetchall()
+# if my_database.is_connected():
+#     cursor = my_database.cursor()
+#     query = "SELECT deal_date FROM Transactions WHERE id_account_emitter = 2;"
+#     cursor.execute(query)
+#     dates = cursor.fetchall()
 
-    final_dates = []
-    for index, date in enumerate(dates):
-        print(index)
-        print(date)
-        final_dates.append(str(date[0]))
-        # date = str(date[0])
-        # date = date[0]    
-    print(final_dates)
+#     final_dates = []
+#     for index, date in enumerate(dates):
+#         print(index)
+#         print(date)
+#         final_dates.append(str(date[0]))
+#         # date = str(date[0])
+#         # date = date[0]    
+#     print(final_dates)
 
-    datetime_str = '2024-06-09'
+#     datetime_str = '2024-06-09'
 
-    dateobject = datetime.strptime(datetime_str, '%y-%m-%d')
-    print(dateobject)
+#     dateobject = datetime.strptime(datetime_str, '%y-%m-%d')
+#     print(dateobject)
 
 
     # datetime_str = '09/19/22 13:55:26'
@@ -58,3 +58,27 @@ if my_database.is_connected():
     # f"{description}, {amount }, " +
     # f"{deal_date}, {deal_type}, " +
     # f"{category});")
+
+date = "2025-03-03"
+date_string = date.split("-")
+year = int(date_string[0])
+month = int(date_string[1])
+day = int(date_string[2])
+
+print(year)
+print(month)
+print(day)
+
+current_time = datetime.now().strftime('%Y-%m-%d').split("-")
+print(current_time)
+
+if year < int(current_time[0]):
+    print("yata")
+else:
+    if month < int(current_time[1]):
+        print("yattaaa")
+    else:
+        if day < int(current_time[2]):
+            print("YATTA")
+
+# print(f"{current_time.year}-{current_time.month}-{current_time.day}")
--- view/transactions.py
+++ view/transactions.py
@@ -158,14 +158,14 @@ class TransactionView(Interface):
     def build_date_entry(self, row1, row2):
         self.deal_date_text = customtkinter.CTkLabel(master=self, text="Choisissez la date de votre transactions :", font=self.text_font, text_color=SOFT_YELLOW, bg_color=DARK_BLUE, justify="left", anchor="w")
         self.deal_date_text.grid(row=row1, column=0, sticky="sew", padx=20, pady=5)
+        
         self.chose_date = DateEntry(master=self, date_pattern="yyyy-mm-dd",
             font=self.text_font, text_color=DARK_BLUE, bg_color=DARK_BLUE, fg_color=SOFT_YELLOW,
-            dropdown_fg_color = SOFT_YELLOW, dropdown_text_color = DARK_BLUE,
-            dropdown_font= self.text_font,
             borderwidth=2, bordercolor=DARK_BLUE,
             background=DARK_BLUE, foreground=SOFT_YELLOW, headersbackground=PINK,
             selectbackground=PINK, normalbackground=LIGHT_BLUE, weekendbackground=SOFT_BLUE, corner_radius=10,
             othermonthbackground=SOFT_BLUE2, othermonthwebackground=SOFT_BLUE3, othermonthforeground=DARK_BLUE, othermonthweforeground=DARK_BLUE)
+        
         self.chose_date.grid(row=row2, column=0, sticky="sew", padx=20, pady=5)
 
     def build_calendar(self, row1, row2):
