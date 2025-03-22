72782421745437165249f929ec41741946615d3f
jolyne-mangeot <joseph.mangeot@laplateforme.io>
2025-03-22 16:03:14 +0100

    feat: add historic and dashboard classes to display accounts main informations

--- main_2.py
+++ main_2.py
@@ -4,10 +4,8 @@ import hashlib
 import secrets
 # from view.interface import Interface
 from view.login_out import LogInOut
-from view.transactions import TransactionView
-from controller.transactionmanager import TransactionManager
-from view.interface_frames import Interface_frames
-from view.historic import Historic
+from view.interface import Interface
+from view.dashboard import Dashboard
 from view.__settings__ import DARK_BLUE, SOFT_BLUE, LIGHT_BLUE, YELLOW, SOFT_YELLOW, PINK
 
 from model.server import ServerDatabase
@@ -21,16 +19,10 @@ view = LogInOut("Connexion Client", 0)
 # view_test = TransactionManager("Transactions", 0, 2) # window_title, column_number, current_session (id_user)
 # view_test = TransactionView("Transactions", 0, 2)
 view.mainloop()
-login_view = Historic("HIHIAHHEIAHIIFEZHFEZ", 1)
+interface = Interface("Budget Buddy", 1)
+dashboard = Dashboard(interface)
+dashboard.build_dashboard()
+# transaction = TransactionView(dashboard)
 
-login_view.interface_frame = Interface_frames(login_view, bg_color=DARK_BLUE, fg_color=LIGHT_BLUE, width=200, corner_radius=20)
-login_view.interface_frame.columnconfigure(0, weight=1)
-login_view.interface_frame.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
-login_view.interface_frame.box = customtkinter.CTkLabel(
-    login_view.interface_frame, text="fegfeer", 
-    height=50, width=200, bg_color=DARK_BLUE, corner_radius=20,
-    fg_color=SOFT_BLUE, text_color=LIGHT_BLUE
-)
-login_view.interface_frame.box.grid(row=0, column=0)
 
-login_view.mainloop()
+interface.mainloop()
--- view/interface.py
+++ view/interface.py
@@ -1,15 +1,20 @@
 import customtkinter
 from view.utiltool import UtilTool
 from view.__settings__ import DARK_BLUE, SOFT_BLUE, LIGHT_BLUE, YELLOW, SOFT_YELLOW, PINK
+from view.interface_frames import Interface_frames
+
+from model.server import ServerDatabase
+
 
 class Interface(customtkinter.CTk):
     def __init__(self, window_title, column_number):
         super().__init__()
         self.current_scene = "broad_view"
-        self.geometry("640x480")
+        self.geometry("820x480")
         self.config(background = DARK_BLUE)
         self.title(window_title)
         self.columnconfigure((column_number), weight=1)
         self.util = UtilTool()
         self.title_font = self.util.get_title_font(30)
         self.text_font = self.util.get_text_font(15)
+        self.subtitle_font = self.util.get_text_font(25)
