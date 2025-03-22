22b645055a93a296ff78c8190562b7c7a2b08b06
jolyne-mangeot <jolyne.mangeot@gmail.com>
2025-03-22 21:00:36 +0100

    feat: add filters and queries to transactions historic

--- data_access/account_data_access.py
+++ data_access/account_data_access.py
@@ -103,3 +103,83 @@ class DataAccess():
         database.close()
         return accounts
     
+    def init_historic_queries(self):
+        self.historic_queries_dict : dict = {
+            "Voir tout" : self.historic_query_all,
+            "Par catégorie" : self.historic_query_category,
+            "Par type" : self.historic_query_type,
+            "Par dates" : self.historic_query_dates,
+        }
+    
+    def historic_query_all(self, account_id, none) -> list:
+        database = self.database.database_connection()
+
+        if database.is_connected():
+            cursor = database.cursor()
+            accounts_query = ("""
+                SELECT t.deal_description, t.amount, t.deal_date, t.category
+                FROM Transactions as t
+                INNER JOIN Bank_account as emitter ON t.id_account_emitter = emitter.id_account
+                INNER JOIN Bank_account as receiver ON t.id_account_receiver = receiver.id_account
+                WHERE emitter.id_user =%s
+                OR receiver.id_user =%s;
+                """ if account_id == 0 else """
+                SELECT deal_description, amount, deal_date, category 
+                FROM Transactions 
+                WHERE id_account_emitter =%s or id_account_receiver =%s;
+            """)
+            cursor.execute(accounts_query, (account_id, account_id))
+            accounts = cursor.fetchall()
+            cursor.close()
+        database.close()
+        return accounts
+    
+    def historic_query_category(self, account_id, category) -> list:
+        database = self.database.database_connection()
+
+        if database.is_connected():
+            cursor = database.cursor()
+            accounts_query = """
+            SELECT deal_description, amount, deal_date, category 
+            FROM Transactions 
+            WHERE id_account_emitter =%s or id_account_receiver =%s
+            and category =%s;
+            """
+            cursor.execute(accounts_query, (account_id, account_id, category))
+            accounts = cursor.fetchall()
+            cursor.close()
+        database.close()
+        return accounts
+
+    def historic_query_type(self, account_id, type) -> list:
+        database = self.database.database_connection()
+
+        if database.is_connected():
+            cursor = database.cursor()
+            accounts_query = """
+            SELECT deal_description, amount, deal_date, category 
+            FROM Transactions 
+            WHERE id_account_emitter =%s or id_account_receiver =%s
+            and deal_type =%s;
+            """
+            cursor.execute(accounts_query, (account_id, account_id, type))
+            accounts = cursor.fetchall()
+            cursor.close()
+        database.close()
+        return accounts
+
+    def historic_query_dates(self, account_id, **dates) -> list:
+        database = self.database.database_connection()
+
+        if database.is_connected():
+            cursor = database.cursor()
+            accounts_query = """
+            SELECT deal_description, amount, deal_date, category 
+            FROM Transactions 
+            WHERE id_account_emitter =%s or id_account_receiver =%s;
+            """
+            cursor.execute(accounts_query, (account_id, account_id))
+            accounts = cursor.fetchall()
+            cursor.close()
+        database.close()
+        return accounts
