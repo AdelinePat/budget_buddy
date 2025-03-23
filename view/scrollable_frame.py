d510cc963ddd4e34ef3fb888a2ebe2ee1554a4eb
AdelinePat <adeline.patenne@hotmail.fr>
2025-03-23 15:18:22 +0100

    refactor: scrollable_frame init in interface in self.scrollable_frame attribute

--- view/scrollable_frame.py
+++
@@ -1,6 +0,0 @@
-import customtkinter
-
-class Scrollable_frame(customtkinter.CTkScrollableFrame):
-    def __init__(self, master, **kwargs):
-        super().__init__(master=master, **kwargs)
-        pass
