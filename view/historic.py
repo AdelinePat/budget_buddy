5e8a38f3530d6fe2a57881eb25d619f50e294032
AdelinePat <adeline.patenne@hotmail.fr>
2025-03-25 01:31:51 +0100

    fix: filters display historic

--- view/historic.py
+++ view/historic.py
@@ -4,14 +4,16 @@ from view.account_view import Account_view
 from view.utiltool import UtilTool
 
 class Historic():
-    def __init__(self, master, account_list, display_accounts, account_id):
+    def __init__(self, master, account_list, display_accounts, login_info):
         self.master = master
         self.util = UtilTool()
         self.title_font = self.util.get_title_font(30)
         self.text_font = self.util.get_text_font(15)
         self.subtitle_font = self.util.get_text_font(25)
         
-        self.shown_historic = account_id
+        
+        self.login_info = login_info
+        self.shown_historic = str(self.login_info.get_current_account())
         self.list_accounts = account_list
         self.display_accounts = display_accounts
         self.recover_accounts()
@@ -23,6 +25,9 @@ class Historic():
             height=50, fg_color=SOFT_BLUE, text_color=LIGHT_BLUE
         )
         self.master.historic_title.grid(row=0, column=0, pady=20, sticky="ew")
+        
+        if self.shown_historic == 'None':
+            self.shown_historic = "0"
 
         self.historic_dict_account[self.shown_historic].build(
             self.master,
@@ -33,15 +38,20 @@ class Historic():
     def recover_accounts(self):
         self.historic_dict_account : dict = {}
         for index, account in enumerate(self.list_accounts):
-            historic_account = Account_view(self.list_accounts[index][0])
+            historic_account = Account_view(self.list_accounts[index][0], self.login_info)
             self.historic_dict_account.update(
                 {str(account[0]) : historic_account}
             )
 
     def flip_historic_account(self, choice):
+        
+
         self.historic_dict_account[self.shown_historic].destroy(self.master)
         self.historic_dict_account[self.shown_historic].destroy_factors_block_dict[self.historic_dict_account[self.shown_historic].current_filter]()
+
+        self.login_info.set_current_account(choice[0])
         self.shown_historic = choice[0]
+
         self.historic_dict_account[self.shown_historic].build(
             self.master,
             self.display_accounts[list(self.historic_dict_account.keys()).index(self.shown_historic)],
